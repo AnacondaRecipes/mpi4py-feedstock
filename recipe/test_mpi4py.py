63e3a940797a95213d38a5ca71a75f167e4caf42
Eric Lundby <Eric.Lundby@gmail.com>
2022-12-16 12:59:07 -0700

    Update tests with Windows specific tests

--- recipe/test_mpi4py.py
+++ recipe/test_mpi4py.py
@@ -4,15 +4,23 @@ import mpi4py
 from mpi4py import MPI
 import mpi4py.futures
 
-# on Windows this checks MSMPI_VER
-print(MPI.get_vendor())
+vendor, version = MPI.get_vendor()
+print(vendor, version)
 
 print(MPI.Get_library_version())
 
 # verify get_config paths exist (not links to build env)
 config = mpi4py.get_config()
 print(f"config={config}")
-for name in ("mpicc", "mpicxx", "mpifort"):
+
+if vendor == 'Microsoft MPI':
+    # https://github.com/erdc/mpi4py/blob/cf9bace83350fcd0db5b359e85402e2197d185fe/mpi.cfg#L173
+    config_path_keys = ('include_dirs', 'library_dirs')
+else:
+    config_path_keys = ("mpicc", "mpicxx", "mpifort")
+
+
+for name in config_path_keys:
     assert name in config
     path = config[name]
     assert os.path.exists(path), f"{name} missing: {path}"
