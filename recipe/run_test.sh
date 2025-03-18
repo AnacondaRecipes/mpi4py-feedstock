e005dd52808ed9b7feed2b22950627f4aa29c3f2
Serhii Kupriienko <skupriienko@anaconda.com>
2025-03-18 19:18:22 +0200

    test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 set -ex
 
 export OMPI_MCA_plm=isolated
@@ -6,9 +8,13 @@ export OMPI_MCA_rmaps_base_oversubscribe=yes
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 
+# Basic tests
+mpiexec -n 1 python -m mpi4py --version
+mpiexec -n 1 python -m mpi4py --mpi-std-version
+mpiexec -n 1 python -m mpi4py --mpi-lib-version
 mpiexec -n 2 python -m mpi4py.bench helloworld
 mpiexec -n 2 python -m mpi4py.bench ringtest
 
-python ${RECIPE_DIR}/test_mpi4py.py
+#python ${RECIPE_DIR}/test_mpi4py.py
 
 pip check
