5530a85dd20160672b857451371a444e6a91d695
PatrykKups <pkups@anaconda.com>
2025-06-04 12:53:09 +0200

    PKG-8331: mpi4py 4.0.3

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -4,6 +4,15 @@ set OMPI_MCA_rmaps_base_oversubscribe=yes
 set OMPI_ALLOW_RUN_AS_ROOT=1
 set OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 
+mpiexec -n 1 python -m mpi4py --version
+if errorlevel 1 exit 1
+
+mpiexec -n 1 python -m mpi4py --mpi-std-version
+if errorlevel 1 exit 1
+
+mpiexec -n 1 python -m mpi4py --mpi-lib-version
+if errorlevel 1 exit 1
+
 mpiexec -n 2 python -m mpi4py.bench helloworld
 if errorlevel 1 exit 1
 
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,14 +1,12 @@
+#/bin/sh
 set -ex
 
-export OMPI_MCA_plm=isolated
-export OMPI_MCA_btl_vader_single_copy_mechanism=none
-export OMPI_MCA_rmaps_base_oversubscribe=yes
-export OMPI_ALLOW_RUN_AS_ROOT=1
-export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
-
+# Basic tests
+mpiexec -n 1 python -m mpi4py --version
+mpiexec -n 1 python -m mpi4py --mpi-std-version
+mpiexec -n 1 python -m mpi4py --mpi-lib-version
 mpiexec -n 2 python -m mpi4py.bench helloworld
 mpiexec -n 2 python -m mpi4py.bench ringtest
 
+# Additional tests are in Python
 python ${RECIPE_DIR}/test_mpi4py.py
-
-pip check
